bb18c73e2ae8ec0ce3cf3190c4293d7bdddb868e
eder <edpefal@gmail.com>
2015-11-19 22:12:07 -0600

    push to client

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -5,10 +5,15 @@
   response.success("Hello world!");
 });*/
 
+//actuakizar el estado del request una vez respondido
 Parse.Cloud.define("sendPushToClient", function(request, response) {
+  var userQuery = new Parse.Query(Parse.User);
+  userQuery.equalTo("objectId",request.params.client );
+
   var query = new Parse.Query(Parse.Installation);
   query.equalTo('channels', 'Client');
   query.equalTo("user", request.params.client);
+  query.matchesQuery('user', userQuery);
   Parse.Push.send({
   		where: query, // Set our Installation query
   		data: {
@@ -20,10 +25,12 @@ Parse.Cloud.define("sendPushToClient", function(request, response) {
   		success: function() {
     	// Push was successful
     		console.log("Push was successful");
+        response.success("Push was successful");
   		},
   		error: function(error) {
     	// Handle error
     	console.error("Push error: " + error.code + " : " + error.message);
+      response.error("Push error: " + error.code + " : " + error.message);
   		}
 	});
 });
