9f3a162073f8d1a5c93bd97365e78de242b78c1d
eder <edpefal@gmail.com>
2016-03-03 09:10:33 -0600

    attended by

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -6,13 +6,43 @@
 });*/
 
 //actuakizar el estado del request una vez respondido
+Parse.Cloud.define("sendPushToEmployee", function(request, response) {
+  var userQuery = new Parse.Query(Parse.User);
+  userQuery.equalTo("objectId",request.params.employeeId ); 
+
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo('channels', 'Employee');
+  //query.equalTo("user", request.params.client);
+  query.matchesQuery('user', userQuery);
+  Parse.Push.send({
+      where: query, // Set our Installation query
+      data: {
+        alert: "Nueva solicitud de servicio",
+        homeServiceRequest: request.params.requestId
+        //homeServiceRequest: request.params.requestId
+      }
+  }, {
+      success: function() {
+      // Push was successful
+        console.log("Push was successful");
+        response.success("Push was successful");
+      },
+      error: function(error) {
+      // Handle error
+      console.error("Push error: " + error.code + " : " + error.message);
+      response.error("Push error: " + error.code + " : " + error.message);
+      }
+  });
+});
+
+
 Parse.Cloud.define("sendPushToClient", function(request, response) {
   var userQuery = new Parse.Query(Parse.User);
   userQuery.equalTo("objectId",request.params.client ); 
 
   var query = new Parse.Query(Parse.Installation);
   query.equalTo('channels', 'Client');
-  query.equalTo("user", request.params.client);
+  //query.equalTo("user", request.params.client);
   query.matchesQuery('user', userQuery);
   Parse.Push.send({
   		where: query, // Set our Installation query
@@ -20,7 +50,11 @@ Parse.Cloud.define("sendPushToClient", function(request, response) {
     		alert: request.params.homeServiceName + " atenderá el servicio el " + request.params.date,
     		date: request.params.date,
         title: "Confirmación de servicio",
-        problemDescription: request.params.problemDescription
+        problemDescription: request.params.problemDescription,
+        imageUrl: request.params.imageUrl,
+        attendedBy: request.params.attendedBy,
+        homeServiceName: request.params.homeServiceName,
+        attendedByAvatar: request.params.attendedByAvatar
     		//homeServiceRequest: request.params.requestId
   		}
 	}, {
@@ -47,6 +81,7 @@ Parse.Cloud.afterSave("HomeServiceRequest", function(request) {
 
 
         var query = new Parse.Query(Parse.Installation);
+        query.equalTo("channels","Partner");
         //console.log("request "+ request);
         //var mHomeService = Parse.Object.extend("HomeServices");
         //var mHomeService = request.object.get("homeService").get("name");
@@ -86,7 +121,8 @@ Parse.Cloud.afterSave("Review", function(request) {
 			post.set("rating",request.object.get("numStars"));
 		    post.save();
 		    console.log("REQUEST RATED");
-		    averageRatings(post.get("homeService"));
+		    //averageRatings(post.get("homeService"));
+        averageRatingsForPartner(post.get("attendedBy"));
 		},
 		error: function(error) {
 		     console.error("Got an error saving average" + error.code + " : " + error.message);
@@ -135,5 +171,49 @@ function averageRatings(homeServiceID){
   });
 
 }	
+
+function averageRatingsForPartner(attendedBy){
+
+ //var userQuery = new Parse.Query(Parse.User);
+  //userQuery.equalTo("objectId",attendedBy); 
+
+  query = new Parse.Query("Review");
+  query.equalTo("attendedBy",attendedBy);
+  //query.equalTo("homeService",homeServiceID);
+  query.find({
+    success: function(results) {
+      var average = 0;
+      for (var i = 0; i < results.length; i++) {
+          var object = results[i];
+          average = average + object.get("numStars");
+
+          
+      }
+      //console.log(results);
+      average = average / results.length;
+     
+    var userQuery = new Parse.Query(Parse.User);
+    //userQuery.equalTo("objectId",attendedBy); 
+      query.get(attendedBy, {
+        success: function(post) {
+          post.set("stars",average);
+          //post.set("comments",results.length);
+          post.save();
+           console.log("STATS FOR PARTNER SAVED");
+        },
+        error: function(error) {
+          console.error("Got an error saving average for partner" + error.code + " : " + error.message);
+        }
+      });
+
+    },
+    error: function(error) {
+      console.error("Got an error getting reviews" + error.code + " : " + error.message);
+    }
+  });
+
+} 
   
 });
+
+
