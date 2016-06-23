2b56ea6488434ac26e27593269a850c91736a303
eder <edpefal@gmail.com>
2016-06-22 23:26:47 -0500

    cancel from employee

--- CloudCode/cloud/cloudFunctions.js
+++ CloudCode/cloud/cloudFunctions.js
@@ -29,4 +29,39 @@ Parse.Cloud.define("sendServiceCompletedPush", function(request, response) {
       }
   });
 });
+
+Parse.Cloud.define("sendCancelServicePushToUser", function(request, response) {
+  var userQuery = new Parse.Query(Parse.User);
+  userQuery.equalTo("objectId",request.params.userId ); 
+
+      //response.success("Push was successful");
+
+      Parse.Push.send({
+        where: userQuery, // Set our Installation query
+        data: {
+        alert: "El servicio ha sido cancelado " + post.get('homeService').get('name') 
+        //homeServiceRequest: request.params.requestId
+          //homeServiceRequest: request.params.requestId
+          }
+        },  {
+        success: function() {
+          // Push was successful
+            console.log("Push was successful");
+            response.success("Push was successful");
+        },
+          error: function(error) {
+          // Handle error
+          console.error("Push error: " + error.code + " : " + error.message);
+          response.error("Push error: " + error.code + " : " + error.message);
+        }
+      });
+  
+    },
+    error: function(error) {
+         console.error("Got an error canceling service" + error.code + " : " + error.message);
+          response.error("Got an error canceling service: " + error.code + " : " + error.message);
+    }
+  });
+ 
+});
  
